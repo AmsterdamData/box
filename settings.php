582d3ce594f40792b1034e1a08a181b0b18bfb6d
Jasper Soetendal <jasper@soetendal.nl>
2015-08-06 18:10:16 +0200

    Verwijderen settings.php

--- settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-DEFINE("FTP_DOMAIN", "ftp.box.com");
-DEFINE("FTP_USERNAME", "j.soetendal@braxwell.com");
-DEFINE("FTP_PASSWORD", "XStream12");
-DEFINE("BOX_CLIENT_ID", 'zdfmc5tiatr2gsl6y9m1fx1jpkh1em5a');
-DEFINE("BOX_CLIENT_SECRET", '');
-DEFINE("BOX_REDIRECT_URI", 'https://tools.amsterdamopendata.nl/box/index.php');
-?>
